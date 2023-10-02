99defd8afc352e0fc657576833603c14becfa5e9
vanessachristie <95264849+vanessachristie@users.noreply.github.com>
2023-10-02 13:59:55 +0700

    Initial Commit

---
+++ W3_SimpleCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  W3_SimpleCalculator
+//
+//  Created by MacBook Pro on 02/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
